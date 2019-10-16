ac8a053987a4d4ff9deed0505e210d910877980e
Amosa <amosa.o94@gmail.com>
2019-10-15 21:37:30 -0400

    Messy Tile Palette
    Tiles working. Need to organize tile palette objects
    
    Need to link Player Sprites, Animation and Controls

---
+++ infinite-runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public GameObject m_pauseMenu;
+
+    public bool m_isPaused = false;
+
+    void Awake() 
+    {
+        SetInstance();
+    }
+
+    void SetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PauseGame()
+    {
+        if (!m_isPaused)
+        {
+            Time.timeScale = 0;
+            m_isPaused = true;
+            m_pauseMenu.SetActive(true);
+        }
+    }
+
+    public void UnpauseGame()
+    {
+        if (m_isPaused)
+        {
+            Time.timeScale = 1;
+            m_isPaused = false;
+            m_pauseMenu.SetActive(false);
+        }
+    }
+    
+}
---
+++ infinite-runner/Assets/Scripts/MenuOverlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOverlay : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public GameObject scoresScreen;
+
+    public bool m_pauseScreenIsActive = true;
+    public bool m_scoresScreenIsActive = false;
+
+    public void SwitchToSettingsScreen()
+    {
+        pauseScreen.SetActive(false);
+        scoresScreen.SetActive(true);
+        m_pauseScreenIsActive = false;
+        m_scoresScreenIsActive = true;
+    }
+
+    public void SwitchToPauseScreen()
+    {
+        pauseScreen.SetActive(true);
+        scoresScreen.SetActive(false);
+        m_pauseScreenIsActive = true;
+        m_scoresScreenIsActive = false;
+    }
+}
