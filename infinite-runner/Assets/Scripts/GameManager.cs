87ba5ef06348663cfc1f08de275eb84ffd7b8b2c
Amosa <amosa.o94@gmail.com>
2019-10-17 16:21:09 -0400

    Art and Animation
    Prefabs for collectibles (no scripts)
    Prefabs for hazards (needs scripts)
    Level Lighting
    Movement and animations

--- infinite-runner/Assets/Scripts/GameManager.cs
+++ infinite-runner/Assets/Scripts/GameManager.cs
@@ -6,12 +6,26 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
     public GameObject menuOverlay;
+    public GameObject bulletPrefab;
+    public GameObject coinPrefab;
+    public GameObject rareCoinPrefab;
+    public GameObject blockPrefab;
+    public GameObject smallPointsPrefab;
+    public GameObject largePointsPrefab;
+    public GameObject debrisPrebab;
+    public GameObject[] ImageLoop;
+    public float imageLoopSpeed;
 
     public bool isPaused = false;
 
     void Awake() 
     {
         SetInstance();
+    }
+
+    void Start() 
+    {
+        imageLoopSpeed = 2f;
     }
 
     void SetInstance()
--- infinite-runner/Assets/Scripts/MarioController.cs
+++ infinite-runner/Assets/Scripts/MarioController.cs
@@ -5,52 +5,126 @@ using UnityEngine;
 public class MarioController : MonoBehaviour
 {
     public GameManager gameManager;
+    public Animator marioAnimator;
+    public Rigidbody2D rigidBody;
+
     public float MARIO_SPEED;
     public float JUMP_FORCE;
-    public float HOVER_FORCE;
 
-    public Animator marioAnimator;
+    public int pointsEarned;
 
+    public bool grounded;
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void AnimBoolReset()  //Reset mario to Idle Stance
     {
         marioAnimator.SetBool("MarioIsRunning", false);
         marioAnimator.SetBool("MarioIsDucking", false);
         marioAnimator.SetBool("MarioIsAirborne", false);
         marioAnimator.SetBool("MarioJump", false);
-        marioAnimator.SetBool("MarioStandStill", true);
+        marioAnimator.SetBool("MarioStandStill", false);
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
+    void Start()
+    {
+        JUMP_FORCE = 245f;
+        MARIO_SPEED = 1f;
+        
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        marioAnimator.SetBool("MarioIsRunning", false);
+        marioAnimator.SetBool("MarioIsDucking", false);
+        marioAnimator.SetBool("MarioIsAirborne", false);
+        marioAnimator.SetBool("MarioJump", false);
+        marioAnimator.SetBool("MarioStandStill", false);
+
+        pointsEarned = 0;
+    }
+
     void Update()
     {
-        if (Input.GetAxis("Horizontal") > 0)
+        if (!gameManager.isPaused)
         {
-            marioAnimator.SetBool("MarioIsRunning", true);
+            // if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && !grounded)
+            // {
+            //     AnimBoolReset();
+            // }
+
+            if (!grounded)
+            {
+                marioAnimator.SetBool("MarioIsAirborne", true);
+            }
+            else
+            {
+                marioAnimator.SetBool("MarioIsAirborne", false);
+                marioAnimator.SetBool("MarioJump", false);
+            }
+
+            //Animator Bools and Movement
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                if (marioAnimator.GetBool("MarioStandStill")) 
+                { 
+                    marioAnimator.SetBool("MarioStandStill", false); 
+                }
+
+                if (grounded)
+                {
+                    marioAnimator.SetBool("MarioIsRunning", true);
+                }
+
+                transform.Translate(Vector2.right * MARIO_SPEED * Time.deltaTime);
+            }
+            else if (Input.GetAxis("Horizontal") <= 0)
+            {
+                if (marioAnimator.GetBool("MarioIsRunning"))
+                {
+                    marioAnimator.SetBool("MarioIsRunning", false);
+                }
+                if (grounded)
+                {
+                    marioAnimator.SetBool("MarioStandStill", true);
+                }
+                
+                transform.Translate(Vector2.left * MARIO_SPEED * Time.deltaTime);
+            }
+            
+            if (Input.GetKey(KeyCode.W) && grounded)
+            {
+                if (!marioAnimator.GetBool("MarioIsAirborne"))
+                {
+                    marioAnimator.SetBool("MarioJump", true);
+                }
+                rigidBody.AddRelativeForce(Vector2.up * JUMP_FORCE);
+                grounded = false;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                rigidBody.gravityScale = 0.5f;
+            }
+            else
+            {
+                rigidBody.gravityScale = 1f;
+            }
+        }
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.layer == 8) 
+        {
+            grounded = true;
         }
         else
         {
-            marioAnimator.SetBool("MarioIsRunning", false);
+            grounded = false;
         }
-
-        if (Input.GetAxis("Vertical") > 0)
+    
+        if (other.gameObject.tag == "mushroom")
         {
-            marioAnimator.SetBool("MarioJump", true);
+            pointsEarned += 100;
         }
-
-        if (Input.GetAxis("Horizontal") <= 0)
-        {
-            marioAnimator.SetBool("MarioIsRunning", false);
-            marioAnimator.SetBool("MarioStandStill", true);
-        }
-        
-
-        if (!gameManager.isPaused)
-        {
-
-        }
-
     }
 }
